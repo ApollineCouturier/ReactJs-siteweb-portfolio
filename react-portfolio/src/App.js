9a4823c75bd51e41a15c16bf5785359efcf57a6e
ApollineCouturier <62022862+ApollineCouturier@users.noreply.github.com>
2021-03-16 18:39:53 +0100

    finish navbar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import NavBar from './Components/NavBar'
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <div className="sidebar"></div>
-      <div className="main-content"></div>
+      <div className="sidebar">
+        <NavBar />
+      </div>
+      <div className="main-content">
+        <div className="content">
+          <HomePage />
+        </div>
+      </div>
     </div>
   );
 }
--- react-portfolio/src/Pages/HomePage.js
+++ react-portfolio/src/Pages/HomePage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import {Link} from 'react-router-dom';
 
 function HomePage() {
     return(
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am
+                    <span>Lorem Ipsum.</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
+                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
+                    A sint ipsam earum nemo omnis et.
+                </p>
+                <div className="icons">
+                    <Link>
+                        <FontAwesomeIcon icon={faFacebook}/>
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }
+
+export default HomePage;
