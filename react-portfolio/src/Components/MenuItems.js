5c5a7434a8bd4f5dc0d9478a3872046ef79652dd
ApollineCouturier <62022862+ApollineCouturier@users.noreply.github.com>
2021-03-30 23:33:30 +0200

    finish portfolios part, finish all web site, now start mobile part

---
+++ react-portfolio/src/Components/MenuItems.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function MenuItems({menuItem}) {
+    return (
+        <div className="portfolis">
+            {
+                menuItem.map((item)=>{
+                    return <div className="portfolio" key={item.id}>
+                        <div className="image-data">
+                            <img src={item.image} alt=""/>
+                            <ul className="hover-items">
+                                <li>
+                                    <a href={item.link1}>{item.icon1}</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="infoItem">
+                            <h5>
+                                {item.title}
+                            </h5>
+                            <br/>
+                            <hr />
+                            <br />
+                            <p> {item.paragraphe}</p>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
+    )
+}
+
+export default MenuItems;
---
+++ react-portfolio/src/Components/allportfolios.js
@@ -0,0 +1,68 @@
+import port1 from '../img/portImages/maya-1.jpg';
+import port2 from '../img/portImages/port1.jpg';
+import port3 from '../img/portImages/port2.jpg';
+import port4 from '../img/portImages/port3.jpg';
+import port6 from '../img/portImages/port5.jpg';
+import port7 from '../img/portImages/port6.jpg';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'Animation',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port1,
+        title: 'Site web Designe',
+        paragraphe:'Changer le design de l\' ancien siteweb et rajouter la possibilité de télécharger leur catalogue'
+    },
+    {
+        id: 2,
+        category: 'Java',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port2,
+        title: 'Python Web Scrapping'
+    },
+    {
+        id: 3,
+        category: 'Animation',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port3,
+        title: 'Autodesk Maya'
+    },
+    {
+        id: 4,
+        category: 'Javascript',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port4,
+        title: 'Vanilla Javascript Website'
+    },
+    {
+        id: 5,
+        category: 'React Js',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port6,
+        title: 'Getting Started With React'
+    },
+    {
+        id: 6,
+        category: 'React Js',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port7,
+        title: 'Vanilla Javascript Website'
+    },
+    {
+        id: 7,
+        category: 'Test Port',
+        link1: 'www.youtube.com',
+        icon1: 'Github',
+        image: port4,
+        title: 'Vanilla Javascript Website'
+    }
+]
+
+export default portfolios;
