c81e998b7edcf7bd009d7c3ccfbc4f520ea56f7a
ApollineCouturier <62022862+ApollineCouturier@users.noreply.github.com>
2021-04-03 16:21:48 +0200

    start new design

--- react-portfolio/src/Components/NavBar.js
+++ react-portfolio/src/Components/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import avatar from '../img/bgsky.jpg';
+import avatar from '../img/logoApolline.jpg';
 import {NavLink} from 'react-router-dom';
+import {faCodepen, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Navbar() {
     return (
@@ -32,7 +35,20 @@ function Navbar() {
                         </NavLink>
                     </li>
                 </ul>
-
+                <div className="icons">
+                    <a href="https://www.instagram.com/apollinecgdev/" target="_blank" rel="noreferrer" className="icon-holder">
+                        <FontAwesomeIcon icon={faInstagram} className="icon insta" />
+                    </a>
+                    <a href="https://github.com/ApollineCouturier" target="_blank" className="icon-holder" rel="noreferrer">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                    </a>
+                    <a href="https://codepen.io/apollinecouturier/" target="_blank" rel="noreferrer" className="icon-holder">
+                        <FontAwesomeIcon icon={faCodepen} className="icon cp"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/apolline-couturier-gauducheau-298572180/"  target="_blank" rel="noreferrer" className="icon-holder">
+                        <FontAwesomeIcon icon={faLinkedin} className="icon link"/>
+                    </a>
+                </div>
                 <footer className="footer">
                     <p>
                     </p>
--- react-portfolio/src/Pages/HomePage.js
+++ react-portfolio/src/Pages/HomePage.js
@@ -18,20 +18,7 @@ function HomePage() {
                     Je suis à la recherche d'un stage de 3-4 mois d'Avril à Août (libre le 29 mars)<br />
                     Mon objectif est de travailler dans le développement de logiciels, de sites web et d’applications mobiles.<br />
                 </p>
-                <div className="icons">
-                    <a href="https://www.instagram.com/apollinecgdev/" target="_blank" rel="noreferrer" className="icon-holder">
-                        <FontAwesomeIcon icon={faInstagram} className="icon insta" />
-                    </a>
-                    <a href="https://github.com/ApollineCouturier" target="_blank" className="icon-holder" rel="noreferrer">
-                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
-                    </a>
-                    <a href="https://codepen.io/apollinecouturier/" target="_blank" rel="noreferrer" className="icon-holder">
-                        <FontAwesomeIcon icon={faCodepen} className="icon cp"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/apolline-couturier-gauducheau-298572180/"  target="_blank" rel="noreferrer" className="icon-holder">
-                        <FontAwesomeIcon icon={faLinkedin} className="icon link"/>
-                    </a>
-                </div>
+                <br/>
             </header>
         </div>
     )
